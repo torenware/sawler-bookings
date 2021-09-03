5d0f69196dabdeff541c4335609f9fce650205c7
Rob Thorne <rob@torenware.com>
2021-09-03 16:04:47 -0700

    Add population tables for rooms and restriction types

---
+++ migrations/20210903230001_populate_lookup_tables.postgres.down.sql
@@ -0,0 +1,2 @@
+delete from public.restrictions;
+delete from public.rooms;
---
+++ migrations/20210903230001_populate_lookup_tables.postgres.up.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.restrictions (restriction_name) VALUES
+	 ('Reservation'),
+	 ('By Owner');
+
+INSERT INTO public.rooms (room_name) VALUES
+	 ('General''s Quarters'),
+	 ('Major''s Suite');
